55aa3f2511eca86c0b6c40234cdd23403372419b
Patrick Tisdel <patricktisdel@outlook.com>
2018-07-17 11:53:11 -0400

    Added first unbundled draft of Chrome extension.

---
+++ main.js
@@ -0,0 +1,71 @@
+
+$(document).ready(function() {
+
+    loadData();
+
+    $("input").focusout(function() {
+        saveData();
+    });
+    
+    $(".complete-btn").click(function() {
+       $(this).parent(".field").toggleClass("complete"); 
+        saveData();
+    });
+    
+    $("#clear-all").click(function() {
+       $(".field").each(function(i) {   
+            $(this).children("input").val("");
+            $(this).removeClass("complete");
+       });
+        saveData();
+    });
+    
+    $('input').keypress(function (e) {
+         if(e.which == 13)  // the enter key code
+          {
+              console.log("test");
+              $(this).blur();
+            $(this).parent(".field").next(".field").children("input").focus();
+            return false;  
+          }
+    });   
+
+    function saveData() {
+        var myStorage = window.localStorage;
+
+        $(".field").each(function(i) {                   
+            localStorage.setItem('setGoals_'+i, $(this).children("input").val());
+            localStorage.setItem('setGoals_'+i+"_complete", $(this).hasClass("complete"));
+            console.log($(this).hasClass("complete"));
+        });
+        
+        console.log("Saved.");
+
+    }
+
+    function loadData() {
+        $(".field").each(function(i) {                   
+            $(this).children("input").val(localStorage.getItem('setGoals_'+i));
+            if (localStorage.getItem('setGoals_'+i+"_complete")=="true") {
+                $(this).addClass("complete");
+            console.log(localStorage.getItem('setGoals_'+i+"_complete"));
+            }
+        });
+        
+        console.log("Loaded.");
+    }
+    
+    
+    var el = document.getElementById('inputs');
+    var sortable = Sortable.create(el, {
+        
+        onEnd: function (/**Event*/evt) {
+            saveData();
+        },
+        handle: ".handle",
+        animation: 150
+        
+        
+    });
+
+});
